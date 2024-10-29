d3ce1c0f2476d402de9e68db412fd205c90d74ab
mwalexandra <manik167@gmail.com>
2024-10-29 11:45:40 +0100

    test commit

--- Benutzerverwaltung/Kernel.cs
+++ Benutzerverwaltung/Kernel.cs
@@ -8,15 +8,22 @@ namespace Benutzerverwaltung
     public class Kernel : Sys.Kernel
     {
 
+        string versionString = "Version 0.1";
+        DateTime momentofSTart;
+
         protected override void BeforeRun()
         {
             Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+            momentofSTart = DateTime.Now;
         }
 
         protected override void Run()
         {
             Console.Write("Input: ");
             var input = Console.ReadLine();
+
+
+
             Console.Write("Text typed: ");
             Console.WriteLine(input);
         }
